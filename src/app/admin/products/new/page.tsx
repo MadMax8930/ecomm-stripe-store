2302875dd0cd0218785f43d02c34bb651aa10389
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-04-06 22:20:17 +0200

    product form v1

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,9 @@
+import PageHeader from "@/components/PageHeader";
+import ProductForm from "@/components/ProductForm";
+
+export default function NewProductPage() {
+   return (<>
+     <PageHeader>Add Product</PageHeader>
+     <ProductForm />
+   </>)
+}
