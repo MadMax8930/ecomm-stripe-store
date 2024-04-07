3af31f9d2d0a615f5b1516172f4c059259fb822f
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-04-07 15:33:04 +0200

    use form state

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import db from "@/db/db";
 import fs from "fs/promises";
 import { redirect } from "next/navigation";
+import { File } from "buffer";
 
 const fileSchema = z.instanceof(File, { message: "Required" })
 const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
@@ -16,7 +17,7 @@ const addSchema = z.object({
    image: imageSchema.refine(file => file.size > 0, "Required")
 })
 
-export async function addProduct(formData: FormData) {
+export async function addProduct(prevState: unknown, formData: FormData) {
    // console.log(formData)
    const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
    if (result.success === false) {
@@ -30,16 +31,19 @@ export async function addProduct(formData: FormData) {
    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
 
    await fs.mkdir("public/products", { recursive: true })
-   const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
-   await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+   const imagePath = `public/products/${crypto.randomUUID()}-${data.image.name}`
+   await fs.writeFile(imagePath, Buffer.from(await data.image.arrayBuffer()))
 
-   await db.product.create({ data: {
-      name: data.name,
-      description: data.description,
-      priceInCents: data.priceInCents,
-      filePath,
-      imagePath
-   }})
+   await db.product.create({ 
+      data: {
+        isAvailableForPurchase: false,
+        name: data.name,
+        description: data.description,
+        priceInCents: data.priceInCents,
+        filePath,
+        imagePath
+      }
+   })
 
    redirect("/admin/products")
 }
--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -7,15 +7,18 @@ import { formatCurrency } from "@/lib/formatters";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { addProduct } from "@/app/admin/_actions/products";
+import { useFormState, useFormStatus } from "react-dom";
 
 export default function ProductForm() {
+   const [error, action] = useFormState(addProduct, {})
    const [priceInCents, setPriceInCents] = useState<number>()
 
   return (
-    <form action={addProduct} className="space-y-8">
+    <form action={action} className="space-y-8">
       <div className="space-y-2">
          <Label htmlFor="name">Name</Label>
          <Input type="text" id="name" name="name" required />
+         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
          <Label htmlFor="priceInCents">Price In Cents</Label>
@@ -23,20 +26,30 @@ export default function ProductForm() {
       </div>
       <div className="text-muted-foreground">
          {formatCurrency((priceInCents || 0) / 100)}
+         {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
       </div>
       <div className="space-y-2">
          <Label htmlFor="description">Description</Label>
          <Textarea id="description" name="description" required />
+         {error.description && <div className="text-destructive">{error.description}</div>}
       </div>
       <div className="space-y-2">
          <Label htmlFor="file">File</Label>
          <Input type="file" id="file" name="file" required />
+         {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
          <Label htmlFor="image">Image</Label>
          <Input type="file" id="image" name="image" required />
+         {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
-      <Button type="submit">Save</Button>
+      <SubmitButton />
     </form>
   )
+}
+
+function SubmitButton() {
+   const { pending } = useFormStatus()
+
+   return (<Button type="submit" disabled={pending}>{pending ? 'Saving...' : 'Save'}</Button>)
 }
